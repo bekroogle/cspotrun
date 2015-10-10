3e07db0157d43436c15c46c8856d60d87f45c36d
Benjamin J. Kruger <bekroogle@gmail.com>
2015-10-10 15:48:15 -0500

    Page now runs source code!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,70 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+var parser,
+    sourceCode,
+    pegedit_opts,
+    return_val = [],
+    remove_child_objs,
+    traverse,
+    symbol_table;
+
+$('document').ready( function() {
+  initializeFoundation();
+
+  var editor = initializeAceEditor('editor'),
+      terminal = initializeAceEditor('terminal'),
+      contents_uri = 'https://api.github.com/repos/bekroogle/cspotrun-grammar/contents/cspotrun.pegjs';
+
+  buildParserFromRepo(contents_uri);
+  setAceOptions(editor, terminal);
+
+  initializeButtonHandlers(editor,terminal);
+});
+
+var initializeFoundation = function() {
+  $(document).foundation({
+    offcanvas: {
+      open_method: 'overlap',
+      close_on_click: true
+    }
+  });
+};
+
+var buildParserFromRepo = function(uri) {
+  $.get(uri, function(repo) {
+    parser = PEG.buildParser(atob(repo.content));
+  });
+};
+
+var initializeAceEditor = function(id) {
+  var aceElement = ace.edit(id);
+  $('#' + id).height(window.innerHeight);
+  return aceElement;
+};
+
+var setAceOptions = function(editor, terminal) {
+  setEditorOptions(editor);
+  setTerminalOptions(terminal);
+};
+
+var setEditorOptions = function(id) {
+  id.setTheme('ace/theme/monokai');
+  id.getSession().setMode('ace/mode/python');
+};
+
+var setTerminalOptions = function(id) {
+  id.setTheme('ace/theme/chrome');
+  id.setOption('showGutter', false);
+};
+
+var initializeButtonHandlers = function(input, output) {
+  $('#run-btn').click( function(evt) {
+    evt.preventDefault();
+
+    if (!parser) { buildParserFromRepo(); }
+    runProgram(input,output);
+  });
+};
+
+var runProgram = function(input,output) {
+  var ast = parser.parse(input.getValue());
+  output.setValue(traverse(ast),-1);
+};
